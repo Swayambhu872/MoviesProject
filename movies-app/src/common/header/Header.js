253b3c52adb0482482c809e265955dcd1163ee0b
Swayambhu872 <swayambhunathan@gmail.com>
2021-05-06 20:33:43 +0530

    Added 'required' field validation in username control in login form

--- movies-app/src/common/header/Header.js
+++ movies-app/src/common/header/Header.js
@@ -10,6 +10,7 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import PropTypes from 'prop-types';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 const customStyles = {
     content: {
@@ -37,7 +38,10 @@ class Header extends Component {
         super();
         this.state = {
             modalIsOpen: false,
-            value: 0
+            value: 0,
+            username: "",
+            usernameRequired: "dispNone"
+            
         };
     }
     openModalHandler = () => {
@@ -49,6 +53,13 @@ class Header extends Component {
     tabChangeHandler = (event, value) => {
         this.setState({ value });
     }
+    loginClickHandler = () => {
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) :
+            this.setState({ usernameRequired: "dispNone" });
+    }
+    inputUsernameChangeHandler =(e)=>{
+        this.setState({username: e.target.value})
+    }
     render() {
         return (
             <div>
@@ -58,8 +69,12 @@ class Header extends Component {
                         <Button variant='contained' color='default' onClick={this.openModalHandler}>Login</Button>
                     </div>
                 </header>
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login"
-                    onRequestClose={this.closeModalHandler} style={customStyles}>
+                <Modal
+                    ariaHideApp={false}
+                    isOpen={this.state.modalIsOpen}
+                    contentLabel="Login"
+                    onRequestClose={this.closeModalHandler}
+                    style={customStyles}>
                     <Tabs className='tabs' value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login" />
                         <Tab label="Register" />
@@ -68,13 +83,19 @@ class Header extends Component {
                         <TabContainer>
                             <FormControl required>
                                 <InputLabel htmlFor='username'>Username</InputLabel>
-                                <Input id='username' type='text' />
+                                <Input id='username' type='text' username={this.state.username} 
+                                onChange ={this.inputUsernameChangeHandler}/>
+                                <FormHelperText className={this.state.usernameRequired}>
+                                    <span className='red'>
+                                        required
+                                    </span>
+                                </FormHelperText>
                             </FormControl> <br /> <br />
                             <FormControl required>
                                 <InputLabel htmlFor='password'>Password</InputLabel>
                                 <Input id='password' type='password' />
                             </FormControl> <br /> <br />
-                            <Button variant="contained" color='primary'>LOGIN</Button>
+                            <Button variant="contained" color='primary' onClick={this.loginClickHandler}>LOGIN</Button>
                         </TabContainer>
                     }
                 </Modal>
