b048e41078090376b17e94e47b5813ad88ff9daa
Swayambhu872 <swayambhunathan@gmail.com>
2021-05-10 23:49:14 +0530

    Added 'Details' component which displays the details page

--- movies-app/src/screens/home/Home.js
+++ movies-app/src/screens/home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './Home.css';
 import Header from '../../common/header/Header';
 import { withStyles } from '@material-ui/core/styles';
@@ -19,6 +20,8 @@ import { Checkbox } from '@material-ui/core';
 import ListItemText from '@material-ui/core/ListItemText';
 import artists from '../../assets/artists';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Details from '../../screens/details/Details';
 
 const styles = theme => ({
     root: {
@@ -70,6 +73,12 @@ class Home extends Component {
     artistSelectHandler = event => {
         this.setState({artists: event.target.value});
     }
+    movieClickHandler = (movieId) => {
+            ReactDOM.render(
+                <Details movieId={movieId}/>,
+                document.getElementById('root')
+            );
+    }
     render() {
         const { classes } = this.props;
         return (
@@ -91,7 +100,7 @@ class Home extends Component {
                     <div className="left">
                         <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
                             {moviesData.map(movie => (
-                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                <GridListTile onClick={() => this.movieClickHandler(movie.id)} className="released-movie-grid-item" key={"grid" + movie.id}>
                                     <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
                                     <GridListTileBar
                                         title={movie.title}
@@ -166,7 +175,12 @@ class Home extends Component {
                             InputLabelProps={{shrink: true}}
                             />
                             </FormControl>
-                            
+                            <br /><br />
+                                <FormControl className={classes.formControl}>
+                                    <Button variant="contained" color="primary">
+                                        APPLY
+                                    </Button>
+                                </FormControl>
                             </CardContent>
                         </Card>
                     </div>
